39e0792995dbeaa3a816a072c3c188ed9c8ec801
islamfaiza2809-sys <islamfaiza2809@gmail.com>
2025-12-26 23:42:56 +0500

    Add authentication middleware for route protection

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decoded.id).select("-password");
+  next();
+};
+
+export const adminOnly = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: "Admin only" });
+  }
+};
